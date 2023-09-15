efa0d4816ac84d3c883890fb0b870817d93becdd
Andre Venditti <am7venditti@gmail.com>
2023-09-15 02:06:06 -0300

    Adição da navegação lateral estática e icones nos assets

---
+++ src/components/sideNavigation/index.jsx
@@ -0,0 +1,29 @@
+import { Stack } from "@mui/material";
+import chargesIcon from "../../assets/icons/charge.svg";
+import clientsIcon from "../../assets/icons/clients.svg";
+import homeIcon from "../../assets/icons/home.svg";
+import IconMenu from "../IconMenu";
+import "./style.css";
+
+function LateralMenu() {
+  return (
+    <Stack
+      direction="column"
+      alignItems="center"
+      justifyContent="start"
+      paddingTop="45px"
+      gap="72px"
+      sx={{
+        width: 108,
+        height: "100vh",
+        backgroundColor: "var(--gray-200)",
+      }}
+    >
+      <IconMenu icon={homeIcon} name="Home" />
+      <IconMenu icon={clientsIcon} name="Clientes" />
+      <IconMenu icon={chargesIcon} name="Cobranças" />
+    </Stack>
+  );
+}
+
+export default LateralMenu;

345414eedc6db31a9c1f35d8fcfe8025cac8ccec
Andre Venditti <am7venditti@gmail.com>
2023-09-15 02:46:46 -0300

    Adicionando arquivo e criando as rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import "./global.css";
+import AllRoutes from "./routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AllRoutes />
+    </BrowserRouter>
   </React.StrictMode>
 );

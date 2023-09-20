f5694f2cd43b0b5ae62d39343ca25baae41ba8a7
higorini <108697425+higorini@users.noreply.github.com>
2023-09-20 19:00:18 -0300

    Criação da pagina e configuração

---
+++ src/pages/Charge/components/ChargeTable/index.jsx
@@ -0,0 +1,9 @@
+function ChargeTable() {
+  return (
+    <div>
+      <h1>Tabela</h1>
+    </div>
+  );
+}
+
+export default ChargeTable;
--- src/pages/Charge/index.jsx
+++ src/pages/Charge/index.jsx
@@ -1,8 +1,38 @@
+import { Stack } from "@mui/material";
+import Header from "../../components/Header";
+import SideNavigation from "../../components/sideNavigation";
+import ChargeTable from "./components/ChargeTable";
+import ClientIcon from "../../assets/icons/clients.svg";
+import "./style.css";
+
 function Charge() {
   return (
-    <div>
-      <h1>Teste</h1>
-    </div>
+    <>
+      <Stack width="100%" direction="row">
+        <SideNavigation />
+        <Stack
+          width="100%"
+          sx={{
+            padding: "40px 32px 40px",
+            backgroundColor: "var(--gray-100)",
+          }}
+          marginLeft="108px"
+        >
+          <Header userName="Lorena" headerTitle="Clientes" />
+
+          <div className="charge__header">
+            <div className="charge__title">
+              <img src={ClientIcon} alt="Icone Cliente" />
+              <h1>Cobranças</h1>
+            </div>
+          </div>
+
+          <div className="charge__main">
+            <ChargeTable />
+          </div>
+        </Stack>
+      </Stack>
+    </>
   );
 }
 
